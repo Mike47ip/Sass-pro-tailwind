a2d60293cfdc09dfbc76694182d876af1cc8ff2e
Mike Dark <agyemankojo47@gmail.com>
2024-01-26 21:18:02 +0000

    Add animation to chat icons

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,6 +12,7 @@ import iphone from "../assets/Images/iphone.png";
 import stairs from "../assets/Images/stairs.png";
 import chat1 from "../assets/Images/chat1.png";
 import chat2 from "../assets/Images/chat2.png";
+import concave from "../assets/Images/concave.png";
 
 const AnimatedDiv = animated.div;
 
@@ -24,17 +25,34 @@ const Body = () => {
   config: { tension: 250, friction: 20 },
  });
 
+ const chat1Props = useSpring({
+  loop: { reverse: true }, // Reverse the animation when reaching the end
+  from: { y: 0 },
+  to: { y: 20 },
+  config: { tension: 70, friction: 20 },
+ });
+
+ const chat2Props = useSpring({
+  loop: { reverse: true }, // Reverse the animation when reaching the end
+  from: { y: 0 },
+  to: { y: -20 },
+  config: { tension: 70, friction: 20 },
+ });
+
  useEffect(() => {
+  let lastScrollTop = 0;
+
   const handleScroll = () => {
    const scrollPosition = window.scrollY;
    const windowHeight = window.innerHeight;
-
-   // Adjust this threshold based on your needs
    const threshold = windowHeight * 0.1;
 
-   // Set isVisible to true when the user scrolls up beyond the threshold,
-   // and set it to false when the user is below the threshold
-   setIsVisible(scrollPosition > threshold);
+   const scrollDirection = scrollPosition > lastScrollTop ? "down" : "up";
+
+   // Set isVisible to true only when scrolling down and beyond the threshold
+   setIsVisible(scrollDirection === "down" && scrollPosition > threshold);
+
+   lastScrollTop = scrollPosition;
   };
 
   // Attach the scroll event listener
@@ -86,7 +104,9 @@ const Body = () => {
       <img className="Figures overflow-hidden" src={Figures} alt="" />
      </div>
     </div>
-    <p className="text-center text-custom-textboard font-inter">Thousands of teams worldwide are using Solo</p>
+    <p className="text-center text-custom-textboard font-inter">
+     Thousands of teams worldwide are using Solo
+    </p>
     <div className="flex justify-center gap-4 pt-5 pb-32">
      <img src={goldlines} alt="" />
      <img src={rotashow} alt="" />
@@ -106,17 +126,34 @@ const Body = () => {
        src={stairs}
        alt=""
       />
-      <img className="relative top-[-120%] z-20" src={chat1} alt="" />
-      <img
-       className="relative top-[-157%] left-[26%] z-20"
-       src={chat2}
-       alt=""
-      />
-      <div className="flex flex-col gap-8 relative top-[-165%] left-[60%] z-20 text-white">
+      <AnimatedDiv
+       style={{
+        ...chat1Props,
+        opacity: isVisible ? 1 : 0,
+        zIndex: isVisible ? 1 : -1,
+       }}
+      >
+       <img className="relative mt-[-700px] z-20" src={chat1} alt="" />
+      </AnimatedDiv>
+      <AnimatedDiv
+       style={{
+        ...chat2Props,
+        opacity: isVisible ? 1 : 0,
+        zIndex: isVisible ? 1 : -1,
+       }}
+      >
+       <img
+        className="relative mt-[-200px] left-[26%] z-20"
+        src={chat2}
+        alt=""
+       />
+      </AnimatedDiv>
+      <img className="ml-[-7%] mt-[7%]" src={concave} alt="" />
+      <div className="flex flex-col gap-8 relative top-[-40%] left-[60%] z-20 text-white">
        <p className="font-inter font-semibold text-5xl">
         Keeping it all <br /> together
        </p>
-       <p className="text-custom-textboard font-inter">
+       <p className="text-custom-textboard font-inter font-bold">
         Just invite your team, Solo does all the <br /> heavy-lifting.
        </p>
       </div>
